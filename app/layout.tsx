28322d73519c905944e60e9e386c000528b0bf45
Николай Артамонов <nikitaartukh@gmail.com>
2026-03-03 13:21:27 +0300

    metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -175,29 +175,27 @@ export default function RootLayout({
           `}
                 </Script>
 
-                {/* Yandex.Metrika counter - Optimized loading */}
-                <Script id="yandex-metrika" strategy="lazyOnload">
-                    {`
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=105894251", "ym");
+                {/* Yandex.Metrika counter - Exact Code */}
+                <script
+                    type="text/javascript"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105894251', 'ym');
 
-            ym(105894251, "init", {
-                  ssr:true,
-                  webvisor:true,
-                  clickmap:true,
-                  ecommerce:"dataLayer",
-                  accurateTrackBounce:true,
-                  trackLinks:true
-            });
-          `}
-                </Script>
+    ym(105894251, 'init', {ssr:true, webvisor:true, clickmap:true, referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+                        `
+                    }}
+                />
+                <noscript><div><img src="https://mc.yandex.ru/watch/105894251" style={{ position: 'absolute', left: '-9999px' }} alt="" /></div></noscript>
+                {/* /Yandex.Metrika counter */}
             </head>
             <body className="bg-[#050505] text-white selection:bg-[#FF2E63] selection:text-white antialiased overflow-x-hidden">
                 {children}
-                <noscript><div><img src="https://mc.yandex.ru/watch/105894251" style={{ position: 'absolute', left: '-9999px' }} alt="" /></div></noscript>
 
                 {/* Глобальные UI компоненты */}
                 <StickyBar />
